8152427878bb8f0ca68152b6e63cd7a3ff9b34d4
Damien Robinson <damien.robinson@pocketpasta.com>
2022-04-09 12:35:24 +1000

    style: lint fix

--- src/module.ts
+++ src/module.ts
@@ -3,31 +3,28 @@ export default function (moduleOptions) {
   // get all options for the module
   const options = {
     ...moduleOptions,
-    ...this.options.daim
+    ...this.options.daim,
   }
 
-  this.nuxt.hook("components:dirs", (dirs) => {
+  this.nuxt.hook('components:dirs', (dirs) => {
     // Add ./components dir to the list
     dirs.push({
-      path: join(__dirname, "components"),
-      prefix: "d",
-    });
-  });
-
+      path: join(__dirname, 'components'),
+      prefix: 'd',
+    })
+  })
 
   // expose the namespace / set a default
   if (!options.namespace) options.namespace = 'daim'
   const { namespace } = options
 
   // add all of the initial plugins
-  const pluginsToSync = [
-    'components/index.js',
-  ]
+  const pluginsToSync = ['components/index.js']
   for (const pathString of pluginsToSync) {
     this.addPlugin({
       src: resolve(__dirname, pathString),
       fileName: join(namespace, pathString),
-      options
+      options,
     })
   }
 }
